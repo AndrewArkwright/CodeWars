4fc910a35201cb5b99812604fcb6b1dfe070576c
Andrew Arkwright <andrew.arkwright@hotmail.com>
2022-04-01 11:15:42 -0400

    Adding fuction that checks a string for how many capitals it has and lists the index of them

--- js/main.js
+++ js/main.js
@@ -319,3 +319,16 @@ function oddOrEven(array) {
       return "odd"
   }
 }
+
+/*
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+*/
+var capitals = function (word) {
+  let array = []
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === word[i].toUpperCase()) { //if uppercase
+      array.push(i)
+    }
+  }
+  return array
+}

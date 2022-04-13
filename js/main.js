a5880b9157a7fd06de989d1486fb982c4726fcaf
Andrew Arkwright <andrew.arkwright@hotmail.com>
2022-04-13 07:47:07 -0400

    add function that returns the max and min number in a string

--- js/main.js
+++ js/main.js
@@ -635,3 +635,17 @@ Note: The function accepts an integer and returns an integer
 function squareDigits(num){
   return parseInt(Array.from(String(num), value => value*value).join(""))
 }
+
+/*
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+All numbers are valid Int32, no need to validate them.
+There will always be at least one number in the input string.
+Output string must be two numbers separated by a single space, and highest number is first.--
+*/
+
+function highAndLow(numbers){
+  let array = numbers.split(" ")
+  let maxNum = Math.max(...array)
+  let minNum = Math.min(...array)
+  return `${maxNum} ${minNum}`
+}

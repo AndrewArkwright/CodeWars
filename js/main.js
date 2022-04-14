626e535a4bece832b632b7017bb1c9334514b1f8
Andrew Arkwright <andrew.arkwright@hotmail.com>
2022-04-14 07:54:57 -0400

    add function that sorts a number by decending order

--- js/main.js
+++ js/main.js
@@ -649,3 +649,13 @@ function highAndLow(numbers){
   let minNum = Math.min(...array)
   return `${maxNum} ${minNum}`
 }
+
+/*
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+*/
+
+function descendingOrder(n){
+  let answer = Array.from(String(n))
+  answer.sort((a, b) => b-a)
+  return Number(answer.join(""))
+}

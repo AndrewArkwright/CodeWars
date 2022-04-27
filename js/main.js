6d12a1403e9ca62d321b9c32b16e878c2b40e64f
Andrew Arkwright <andrew.arkwright@hotmail.com>
2022-04-27 08:23:43 -0400

    Add function that takes in a number and returns its' multiplicative persistence. Also add a function that sums all the numbers in an array except the max and min.

--- js/main.js
+++ js/main.js
@@ -944,3 +944,42 @@ function findShort(s){
   })
   return smallest
 }
+
+/*
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example (Input --> Output):
+
+39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+4 --> 0 (because 4 is already a one-digit number)
+*/
+
+//could have just used reduce to do the multiplication instead
+function persistence(num) {
+  num = num.toString().split("")
+  let max = 0
+  let nextIteration = 1
+  while (num.length > 1) {
+    for (let i = 0; i < num.length; i++) {
+      nextIteration *= num[i]
+    }
+    num = nextIteration.toString().split("")
+    nextIteration = 1
+    max++
+  }
+  return max
+}
+
+/*
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+Mind the input validation.
+*/
+
+function sumArray(array) {
+  if (array === null || array === undefined || array.length < 3) return 0
+  return array.reduce((previous, current) => previous + current, 0) - Math.min(...array) - Math.max(...array)
+}

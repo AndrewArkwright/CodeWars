285b03001aebbbb21a9c985f71694eb61ef4d06c
Andrew Arkwright <andrew.arkwright@hotmail.com>
2022-05-01 23:38:51 -0400

    Add function that reverses every word in a string that has more than four characters

--- js/main.js
+++ js/main.js
@@ -1079,3 +1079,25 @@ function digital_root(n) {
   }
   return n
 }
+
+/*
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
+*/
+
+function spinWords(string){
+  return string.split(" ").map((value, index, array) => {
+    let reverse = ""
+    if (value.length > 4) {
+      for (let i = value.length; i > 0; i--) {
+        reverse += value.charAt(i-1)
+      }
+    value = reverse
+      return value
+    }
+    else {
+      return value
+    }
+  }).join(" ")
+}

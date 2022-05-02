3bc1b27e1e1a7c6ed10dfb80ca6adaf4c056e67a
Andrew Arkwright <andrew.arkwright@hotmail.com>
2022-05-02 07:18:02 -0400

    Add a function that converts a number to binary and adds the bits together

--- js/main.js
+++ js/main.js
@@ -1101,3 +1101,11 @@ function spinWords(string){
     }
   }).join(" ")
 }
+
+/*
+Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+*/
+
+const countBits = n => parseInt(n.toString(2).split("").reduce((a, b) => Number(a) + Number(b)))

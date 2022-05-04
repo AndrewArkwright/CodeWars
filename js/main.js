61147c12a64ecc90d9d12597417a2e78d4a24877
Andrew Arkwright <andrew.arkwright@hotmail.com>
2022-05-04 08:00:32 -0400

    Add function that puts an array of numbers into a phone number string. Add a function that returns the even number in an odd numbered array or the odd number in an even numbered array.

--- js/main.js
+++ js/main.js
@@ -1139,3 +1139,69 @@ function removeSmallest(numbers) {
   }
   return newArray
 }
+
+/*
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+*/
+
+function createPhoneNumber(numbers){
+  //could us a template string and replace the numbers with the array values, but this just seemed easiest
+  return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+}
+
+/*
+You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+*/
+
+function findOutlier(integers){
+  //if there is a odd outlier
+  if (integers.filter(value => value % 2 === 0).length === 1) {
+    return integers.filter(value => value % 2 === 0).reduce((a, b) => a + b)
+  }
+  else {
+    //return even outlier
+    return integers.filter(value => value % 2 != 0).reduce((a, b) => a + b)
+  }
+}
+
+function findOutlier(integers){
+  //even is first
+  if (integers[0] % 2 === 0) {
+    if (integers[1] % 2 === 0) {
+      //search for a odd and first two are even\
+      for (let i = 2; i < integers.length; i++) {
+        console.log(integers[i])
+        if (integers[i] % 2 != 0) {
+          return integers[i]
+        }
+      }
+    }
+    else if (integers[2] % 2 === 0) {
+      //if there is a odd between two even
+      return integers[1]
+    }
+    else {
+      //if first is even and next two are odd
+      return integers[0]
+    }
+  }
+  //odd is first
+  else {
+      if (integers[1] % 2 != 0) {
+        //first two are odd so search for even
+        for (let i = 2; i < integers.length; i++) {
+          if (integers[i] % 2 === 0) {
+            return integers[i]
+          }
+        }
+      }
+      else if (integers[2] % 2 != 0) {
+        //if there is a even between two odd
+        return integers[1]
+      }
+      else {
+        //first is odd and next two are even
+        return integers[0]
+    }
+  }
+}

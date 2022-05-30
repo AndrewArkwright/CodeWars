e85cb10a4b584ca36ce1858ffd8637de9a9f9f03
Andrew Arkwright <andrew.arkwright@hotmail.com>
2022-05-30 07:45:52 -0400

    Add function that returns true of false depending on if the positive number given is a narcissistic number.

--- js/main.js
+++ js/main.js
@@ -2411,3 +2411,18 @@ function getDivisorsCnt(n){
   }
   return count
 }
+
+/*
+A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+
+Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10. This may be True and False in your language, e.g. PHP.
+
+Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
+*/
+function narcissistic(value) {
+  // We are given a positive number and need to figure out if each number to the power of the value length is equal to the number
+  // We return true or false if it is or isn't
+  //1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+  //Since it is a number, we need to convert to string and if we convert to an array, we can map and reduce it to get the sum
+  return String(value).split("").map(val => Number(val) ** String(value).length).reduce((prev, current) => prev + current, 0) === value ? true : false
+}

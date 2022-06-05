285cf1093e8d08723c3603b48e91a14dbf5648ca
andrew arkwright <andrew.arkwright@hotmail.com>
2022-06-05 10:28:44 -0400

    Add function that returns the sum of the perimeter of all squares when given the amount of squares that have the perimeter matching each number of the Fibonacci sequence.

--- js/main.js
+++ js/main.js
@@ -2572,3 +2572,30 @@ function orderWeight(string) {
     else {return sumA - sumB}
   }).join(" ")
 }
+
+/*
+The drawing shows 6 squares the sides of which have a length of 1, 1, 2, 3, 5, 8. It's easy to see that the sum of the perimeters of these squares is : 4 * (1 + 1 + 2 + 3 + 5 + 8) = 4 * 20 = 80
+
+Could you give the sum of the perimeters of all the squares in a rectangle when there are n + 1 squares disposed in such that all the squares are arranged in a rectangle
+
+Hint:
+See Fibonacci sequence
+
+Ref:
+http://oeis.org/A000045
+
+The function perimeter has for parameter n where n + 1 is the number of squares (they are numbered from 0 to n) and returns the total perimeter of all the squares.
+*/
+
+function perimeter(n) {
+  //We are given the number of squares in the rectangle that we use to calculate the Fibonacci sequence with
+  //return the sum of all the Fibonacci numbers * 4 (ie the sum of the perimeters of all squares)
+  //4 * (1 + 1 + 2 + 3 + 5 + 8) = 4 * 20 = 80
+  //first I want to make an array of all the Fibonnaci numbers equal to the number of squares needed and use reduce to add them all up and then * 4
+
+  let fibArray = [0, 1] 
+  for (let i = 0; i < n; i++) {
+    fibArray.push(fibArray[i] + fibArray[i+1])
+  }
+  return fibArray.reduce((prev, current) => prev + current, 0) * 4
+}

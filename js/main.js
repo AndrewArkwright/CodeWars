2ecc14c44f166d3395838d4238e09d81ca52afa0
andrew arkwright <andrew.arkwright@hotmail.com>
2022-06-20 08:15:38 -0400

    Add function that returns the first highest length string from a given array with k consecutive strings.

--- js/main.js
+++ js/main.js
@@ -3133,3 +3133,45 @@ function isPrime(num) {
   }
   return true
 }
+
+/*
+You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+Examples:
+strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+Concatenate the consecutive strings of strarr by 2, we get:
+
+treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+The first that came is "folingtrashy" so 
+longest_consec(strarr, 2) should return "folingtrashy".
+
+In the same way:
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm).
+*/
+
+function longestConsec(strarr, k) {
+  //We are given an array of strings that could be empty. We are also given a number that is a whole number, but can be negative, 0, or positive.
+  //We need to return k consecutive strings in the array such that it is the largest length of k consecutive strings
+  //["zone", "abigail", "theta", "form", "libe", "zas"], 2), "abigailtheta"
+  //So what I want to do is make a nested for loop to create a new array of strings that have all the posibile string combinations in it. After that, I would go through each item in the array and find the max string length in the array
+    if (k > strarr.length || k <= 0) {return ""}
+  
+    let string = "", answers = []
+    for (let i = 0; i < strarr.length - k + 1; i++) {
+      for (let j = i; j < i + k; j++) {
+        string += strarr[j]
+      }
+      answers.push(string)
+      string = ""
+    }
+  
+  return answers.reduce((a, b) => a.length >= b.length ? a : b)
+}

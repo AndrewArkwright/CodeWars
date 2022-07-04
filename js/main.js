80793c1d2d154a88c707d492c17a389579f969ce
andrew arkwright <andrew.arkwright@hotmail.com>
2022-07-04 08:27:04 -0400

    Add a function that calculates the amount of people on a bus after we add and remove people per bus stop represented using a matrix.

--- js/main.js
+++ js/main.js
@@ -3571,3 +3571,26 @@ Note: keep the original order of the names in the output.
 //(["Ryan", "Kieran", "Mark"]), ["Ryan", "Mark"]
 //Decided to just use an arrow function and the array method filter to get all the strings I need
 const friend = friends => friends.filter(value => value.length === 4)
+
+/*
+There is a bus moving in the city, and it takes and drop some people in each bus stop.
+
+You are provided with a list (or array) of integer pairs. Elements of each pair represent number of people get into bus (The first item) and number of people get off the bus (The second item) in a bus stop.
+
+Your task is to return number of people who are still in the bus after the last bus station (after the last array). Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and they are probably sleeping there :D
+
+Take a look on the test cases.
+
+Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the return integer can't be negative.
+
+The second value in the first integer array is 0, since the bus is empty in the first bus stop.
+*/
+
+//We are given an matrix such that each column has two numbers in the array, one for the amount of passengers that get on the bus and one for the amount of passengers that get off
+//We return the number of people left on the bus after all columns have been counted
+//[[10,0],[3,5],[5,8]]), 5
+//Decided to use array.map to get the total amount of people we add or subtract from the total and then used reduce to add up each total
+
+var number = function(busStops){
+  return busStops.map(value => value[0] - value[1]).reduce((prev, curr) => prev + curr, 0)
+}

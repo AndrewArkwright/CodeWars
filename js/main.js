8642a0a046d21ad7cc6f59ef9a5bdccb08f54272
andrew arkwright <andrew.arkwright@hotmail.com>
2022-07-06 14:55:00 -0400

    Manipulated substrings of varying sizes depending on a conditon and returned the concatination

--- js/main.js
+++ js/main.js
@@ -3607,3 +3607,35 @@ function divisors(integer) {
   if (answer.length === 0) {return `${integer} is prime`}
   return answer
 }
+
+/*
+The input is a string str of digits. Cut the string into chunks (a chunk here is a substring of the initial string) of size sz (ignore the last chunk if its size is less than sz).
+
+If a chunk represents an integer such as the sum of the cubes of its digits is divisible by 2, reverse that chunk; otherwise rotate it to the left by one position. Put together these modified chunks and return the result as a string.
+
+sz is <= 0 or if str is empty return ""
+sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
+
+Examples:
+revrot("123456987654", 6) --> "234561876549"
+revrot("123456987653", 6) --> "234561356789"
+*/
+
+//We are given a string of numbers and a number (sz)
+//We return a string such that every substring of size sz is checked to see if the sum of each digit cubed in the substring is even or odd and if it is odd, we shift that substring to the left, else we reverse the entire substring. We then return the concatination of each of those substring of size sz even the given string is longer.
+//revrot(s, 5), "330479108928157"
+//I decided to do a for loop x times where x is the amount of times sz goes into str.lenght rounded down so we know how many times we need to check each substring. I then checked if the sum of each digit cubed is odd or even and shifted or reversed the string depending on the results and pushed it into an array. I then joined the array and return the string
+
+function revrot(str, sz) {
+  if (sz === 0 || str.length < sz) {return ""}
+  let count = 0, answer = []
+  
+  for (let i = 0; i < Math.floor(str.length/sz); i++) {
+    let sub = str.substring(count, count + sz)
+    if ((sub.split("").reduce((prev, curr) => prev + Math.pow(Number(curr), 3), 0)) % 2 != 0) {answer.push(sub.substring(1, sz) + sub.charAt(0))}
+    else {answer.push(sub.split("").reverse().join(""))}
+    count += sz
+  }
+  
+  return answer.join("")
+}
